e7e766b745570bd817511643c4b8ba1960f8a36f
leo <leommresende@gmail.com>
2026-07-10 15:51:12 -0300

    O nome é adicionado e é dada a situação

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 nome_aventureiro = None
 def main():
     print("Ola aventureiro! Seja bem vindo!")
-    input("Digite seu nome aqui, aventureiro: ")
+    nome_aventureiro = input("Digite seu nome aqui, aventureiro: ")
+    print(f"Bem vindo, {nome_aventureiro}! Precisamos de sua ajuda para derrotar o rei tiranico que maltrata o reino de Powertronic, nosso reino.")
+
+main()
