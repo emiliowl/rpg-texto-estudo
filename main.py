2c5b217ca1ce40f4d08ecc081e01f307cf9dc280
Emilio Resende <emilio.resende@impacta.edu.br>
2026-07-10 16:01:15 -0300

    melhora captura nome

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 nome_aventureiro = None
+itens = []
+
+textao_bem_vindo = """
+Bem vindo, {nome_aventureiro}! 
+Precisamos de sua ajuda para derrotar o rei tiranico que maltrata o 
+reino de Powertronic, nosso reino.
+"""
+
+def imprime_textao(textao, parametros):
+    print(textao.format(**parametros))
+
 def main():
     print("Ola aventureiro! Seja bem vindo!")
     nome_aventureiro = input("Digite seu nome aqui, aventureiro: ")
-    print(f"Bem vindo, {nome_aventureiro}! Precisamos de sua ajuda para derrotar o rei tiranico que maltrata o reino de Powertronic, nosso reino.")
+    imprime_textao(textao_bem_vindo, {"nome_aventureiro": nome_aventureiro})
 
 main()
