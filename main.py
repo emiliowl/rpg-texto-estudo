e6e254e8debf05d3e67a3b73303c13b7d006f195
leo <leommresende@gmail.com>
2026-07-10 18:16:06 -0300

    adiciona música e inicia a primeira batalha
    Vcs poderão adicionar a música que quiserem, basta vcs baixarem ela em mp3 e mudarem seus nomes para "musica-abertura" ou "luta1" (não sei como mandar as músicas)

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+import pygame
+import time
+
 nome_aventureiro = None
 classe = None
 itens = []
 tem_erro = False
-missao_inicial = "Olá novamente! Irei lhe passar sua \nprimeira missão, portanto espero que esteja preparado!Sua missão é ir até a \nmontanha de Corcovado, Powerpoint, que é o reino vizinho aonosso. Lá é onde se \nlocalizam os goblins, servos do rei tiranico queatormenta nossa cidade...!Mas \napresse-se, os goblins estão a caminho daqui e chegam em algumas horas!"
+missao_inicial = "\nOlá novamente! Irei lhe passar sua \nprimeira missão, portanto espero que esteja preparado!Sua missão é ir até a \nmontanha de Corcovado, Powerpoint, que é o reino vizinho ao nosso. Lá é onde se \nlocalizam os goblins, servos do rei tiranico que atormenta nossa cidade...Mas \napresse-se, os goblins estão a caminho daqui e chegam em algumas horas!"
+
 
 textao_bem_vindo = """
 Bem vindo, {nome_aventureiro}! 
 Precisamos de sua ajuda para derrotar o rei tiranico que maltrata o 
 reino de Powertronic, nosso reino.
 """
+
+
+
+
 
 def imprime_textao(textao, parametros):
     print(textao.format(**parametros))
@@ -22,7 +30,13 @@ def main():
     global classe
     global tem_erro
     global missao_inicial
+    global fase
 
+    pygame.init()
+
+    pygame.mixer.music.load("musica-abertura.mp3")
+    pygame.mixer.music.play()
+    
     print("Ola aventureiro! Seja bem vindo!")
     nome_aventureiro = input("Digite seu nome aqui, aventureiro: ")
     imprime_textao(textao_bem_vindo, {"nome_aventureiro": nome_aventureiro})
@@ -37,16 +51,27 @@ def main():
 
 
     if classe == "guerreiro":
-        itens.append("espada")
-        itens.append("escudo")
+        itens.append("espada: 10 de dano")
+        itens.append("escudo: -10 no dano causado (propriedade especial: ele pode te curar se o dano causado for menor do que a defesa dele)")
     elif classe == "mago":
-        itens.append("cajado")
+        itens.append("cajado: possui a magia bola de fogo: 20 de dano")
         itens.append("capa")
 
     print("Boa escolha!")
     mostra_itens()
-    print(missao_inicial)
+    input("Pressione qualquer enter para prosseguir")
 
+    print(missao_inicial)
+    input("Pressione qualquer enter para prosseguir")
+
+    pygame.mixer.music.stop
+    
+    pygame.mixer.music.load("luta1.mp3")
+    pygame.mixer.music.play()
+    time.sleep(1.5)
+
+    print("Você se depara com 3 globins enquanto escala a montanha de Corcovado, o que deseja fazer? (Seu HP está em 100)")
+    acao_realizada = input("1 - Correr \n2 - Atacar \n3 - defender \n")
 
 
 main()
