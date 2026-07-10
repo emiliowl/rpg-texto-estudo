fa3fb8c7b6998303a288d2c06aa9b29f1b59a048
murdog <mmmurta.resende@gmail.com>
2026-07-10 16:30:46 -0300

    escolha guerreiro ou mago

--- main.py
+++ main.py
@@ -17,12 +17,15 @@ def mostra_itens():
         print(f"- {item}")
 
 def main():
+    global classe
     print("Ola aventureiro! Seja bem vindo!")
     nome_aventureiro = input("Digite seu nome aqui, aventureiro: ")
     imprime_textao(textao_bem_vindo, {"nome_aventureiro": nome_aventureiro})
 
     print(f"{nome_aventureiro}, voce quer armas de guerreiro ou mago?")
-    classe = input("Digite 'guerreiro' ou 'mago': ")
+    while classe not in ["guerreiro", "mago"]:
+        classe = input("Digite 'guerreiro' ou 'mago': ")
+
     if classe == "guerreiro":
         itens.append("espada")
         itens.append("escudo")
